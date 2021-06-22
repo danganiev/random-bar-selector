e287ecf36df40fdfcef761b6a629338d6dd25c17
Dan Ganiev <dan@danganiev.me>
2021-06-22 10:27:59 +0300

    Eslint and prettier work with TS

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  // plugins: [
+  //   "react-app",
+  //   "react-app/jest",
+  //   '@typescript-eslint',
+  // ],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+};
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("app has header with its name", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const headingElement = screen.getByText(/Barhopping Hare/i);
+
+  expect(headingElement).toBeInTheDocument();
 });
+
+test("app has a search form", () => {
+  const searchForm = screen.getByRole("search", { name: /search form/i });
+  expect(searchForm).toBeInTheDocument();
+});
+
+// test('app has a city input', () => {
+//   const cityInput = screen.getByLabelText('City')
+// })
+
+// test('app has an input for the city', () => {
+//   const cityInput = screen.getByRole('searchbox')
+// })
+// test('app has a choose next bar for me button', () => {})
+// test('when you push the button with empty input it will need validation', () => {})
+// test('when you enter the city it autocompletes', () => {})
+// test('when you choose the autocompleted city and press search, result appears after request', () => {})
